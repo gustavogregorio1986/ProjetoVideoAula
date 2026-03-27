065592e873ccb4cf1f2204e23558344e748abea8
Miguel Marquiori <miguelmarkiori@outlook.com>
2026-03-27 14:36:31 -0300

    Add project files.

---
+++ AcademiaDoNet.CursoCSharp.ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AcademiaDoNet.CursoCSharp.ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomeCorrentista = "Ray carneiro";
+            Console.WriteLine("Nome da variavel: " + nomeCorrentista);
+
+            var nomeCorrentista1 = Console.ReadLine();
+            Console.WriteLine("Nome da variavel: " + nomeCorrentista);
+        }
+    }
+}
